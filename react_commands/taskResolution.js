b2ad9ea6a7523ba0fbe3989f2281d64a66abf234
jmanaois <100259401+jmanaois@users.noreply.github.com>
2022-12-07 10:38:49 -0800

    update anyagifs

--- react_commands/taskResolution.js
+++ react_commands/taskResolution.js
@@ -10,16 +10,9 @@ module.exports = {
 		var messageData = reaction.message.content.split('**');
 
 
-		const memes = ['https://brobible.com/wp-content/uploads/2020/10/50-best-memes-iq-joke-math.jpg',
-                'https://petpress.net/wp-content/uploads/2019/12/corgi-12.jpg',
-                'https://www.rd.com/wp-content/uploads/2019/04/01-Hilarious-Dog-Memes.jpg',
-                'https://brobible.com/wp-content/uploads/2020/09/50-best-memes-understanding-jokes.jpg',
-				'https://petpress.net/wp-content/uploads/2019/12/corgi-1.jpg',
-				'https://preview.redd.it/p2htn4m2olh21.png?width=960&crop=smart&auto=webp&s=45ff517fec9fce9dee6d3f96bd326259ec5b5a3a',
-				'https://brobible.com/wp-content/uploads/2020/10/50-best-memes-same-tv-shows.jpg',
-				'https://cdn.vox-cdn.com/thumbor/HWmEqvQy7JSOcFqh1mYE8CDyCf0=/0x38:1920x1043/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/15960038/ply_best_memes_2019.jpg',
-				'https://brobible.com/wp-content/uploads/2020/08/50-best-memes-public-vs-headphones.jpg',
-				'https://memesbams.com/wp-content/uploads/2017/10/7-Cute-Cat-Memes.jpg'
+		const anyagif = ['https://thumbs.gfycat.com/AgilePoliticalAfricanparadiseflycatcher-size_restricted.gif',
+                'https://media.tenor.com/bdvOWIGxp1EAAAAC/spy-x-family-spy-family.gif',
+                'https://64.media.tumblr.com/f5d7c34fecdcdffc4b1839c5e74e0830/e12064a52ed0d543-61/s540x810/7c7dab9727f71a3eb000d3dbcca625e636ea74c7.gif',
                 ]
 
 		if(messageData.length < 2){ return; }
@@ -31,10 +24,9 @@ module.exports = {
 		});
 		
 		if(taskReacted){
-			reaction.message.channel.send(' gj you have completed: \"***' + messageData[1] + '***\" sending u peanuts rn in the dms');
-			const meme_num = Math.floor(Math.random() * 5);
-			const meme_link = memes[meme_num];
-			client.users.cache.get(user.id).send("thank god u completed '" + messageData[1] +  "' here's some peanuts lol",{files: [meme_link]});
+            const gif_num = Math.floor(Math.random() * 5);
+			const gif_link = anyagif[gif_num];
+			reaction.message.channel.send(' gj you have completed: \"***' + messageData[1] + '***\"', {files: [gif_link]});
 			await asmt_queue.update({ completed: true }, {
 				where: {
 					asmtname:messageData[1],
@@ -43,5 +35,5 @@ module.exports = {
 			});
 		}
 	
-	}, // end of execute
+	},
 };
