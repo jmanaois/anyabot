fa0fafb535787338a63d08975c7bf69db522d386
jmanaois <100259401+jmanaois@users.noreply.github.com>
2022-12-07 11:02:04 -0800

    add music feature

--- bot.js
+++ bot.js
@@ -1,13 +1,28 @@
 const Discord = require('discord.js');
 const fs = require('fs');
+const { DisTube } = require('distube')
 
 asmt_queue = require('./sqlitedb');
 users = require('./sqlitedb-users');
 
-const client = new Discord.Client();
+const client = new Discord.Client({
+    intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.GuildVoiceStates,
+    Discord.GatewayIntentBits.MessageContent
+    ]
+});
+const config = require('./auth.json')
+
+client.config = require('./auth.json');
 client.commands = new Discord.Collection();
 client.reactCommands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.emotes = config.emoji;
 
+const { SpotifyPlugin } = require('@distube/spotify')
+const { YtDlpPlugin } = require('@distube/yt-dlp')
 const{ prefix, token } = require('./auth.json');
 
 
@@ -81,4 +96,70 @@ client.on('message', message => {
 		message.channel.send('an unknown error occurred.');
 	}
 }); 
+
+client.distube = new DisTube(client, {
+  leaveOnStop: false,
+  emitNewSongOnly: true,
+  emitAddSongWhenCreatingQueue: false,
+  emitAddListWhenCreatingQueue: false,
+  plugins: [
+    new SpotifyPlugin({
+      emitEventsAfterFetching: true
+    }),
+    new YtDlpPlugin()
+  ]
+})
+client.on('messageCreate', async message => {
+    if (message.author.bot || !message.guild) return
+    const prefix = config.prefix
+    if (!message.content.startsWith(prefix)) return
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase()
+    const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
+    if (!cmd) return
+    if (cmd.inVoiceChannel && !message.member.voice.channel) {
+      return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`)
+    }
+    try {
+      cmd.run(client, message, args)
+    } catch (e) {
+      console.error(e)
+      message.channel.send(`${client.emotes.error} | Error: \`${e}\``)
+    }
+  })
+  
+  const status = queue =>
+    `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.names.join(', ') || 'Off'}\` | Loop: \`${
+      queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
+    }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``
+  client.distube
+    .on('playSong', (queue, song) =>
+      queue.textChannel.send(
+        `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${
+          song.user
+        }\n${status(queue)}`
+      )
+    )
+    .on('addSong', (queue, song) =>
+      queue.textChannel.send(
+        `${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+      )
+    )
+    .on('addList', (queue, playlist) =>
+      queue.textChannel.send(
+        `${client.emotes.success} | Added \`${playlist.name}\` playlist (${
+          playlist.songs.length
+        } songs) to queue\n${status(queue)}`
+      )
+    )
+    .on('error', (channel, e) => {
+      if (channel) channel.send(`${client.emotes.error} | An error encountered: ${e.toString().slice(0, 1974)}`)
+      else console.error(e)
+    })
+    .on('empty', channel => channel.send('Voice channel is empty! Leaving the channel...'))
+    .on('searchNoResult', (message, query) =>
+      message.channel.send(`${client.emotes.error} | No result found for \`${query}\`!`)
+    )
+    .on('finish', queue => queue.textChannel.send('Finished!'))
+    
 client.login(token);
